d61c000d91b0c9f40f4f92ea7e92f67fa161aef8
Yannick Funk <yannickfunk@yahoo.de>
2022-12-24 22:29:18 +0100

    Finish inference examples

--- utils/viz.py
+++ utils/viz.py
@@ -3,8 +3,10 @@ import matplotlib.patches as patches
 import torch
 import numpy as np
 from typing import List
-
+import cv2
 from utils.yolo import filter_boxes, nms
+import math
+
 
 CLASSES = (
     "aeroplane",
@@ -34,50 +36,39 @@ def class_to_num(class_str):
     for idx, string in enumerate(CLASSES):
         if string == class_str: return idx
 
+
 def num_to_class(number):
     for idx, string in enumerate(CLASSES):
         if idx == number: return string
     return 'none'
 
 
-def display_result(image: torch.Tensor, output: List[torch.tensor], target=None, file_path=None) -> None:
+def to_uint8_img(img):
+    img *= 255
+    img = img.astype(np.uint8)
+    return img
+
+
+def display_result(image: np.ndarray, output: List[np.ndarray], file_path=None) -> None:
     _, ax = plt.subplots()
 
-    pad = 20
-    image = image.numpy()[0,:].transpose(1,2,0)
-    image = np.pad(image, ((pad, pad), (pad, pad), (0, 0)), mode='constant')
+    bboxes = np.stack(output, axis=0)
     ax.imshow(image)
-    
-    img_shape = 320
-    if output:
-        bboxes = torch.stack(output, dim=0)
-        for i in range(bboxes.shape[1]):
 
-            if bboxes[0,i,-1] > 0:
-                cx = int(bboxes[0,i,0]*img_shape - bboxes[0,i,2]*img_shape/2) + pad
-                cy = int(bboxes[0,i,1]*img_shape - bboxes[0,i,3]*img_shape/2) + pad
+    for i in range(bboxes.shape[1]):
 
-                w = int(bboxes[0,i,2]*img_shape)
-                h = int(bboxes[0,i,3]*img_shape)
+        if bboxes[0, i, -1] > 0:
+            cx = bboxes[0, i, 0] * 320 - bboxes[0, i, 2] * 320 // 2
+            cy = bboxes[0, i, 1] * 320 - bboxes[0, i, 3] * 320 // 2
 
-                rect = patches.Rectangle((cx,cy),
-                                        w, h, linewidth=2, facecolor='none', edgecolor='r')
-                ax.add_patch(rect)
-                ax.annotate(num_to_class(int(bboxes[0,i,5])) + " "+  f"{float(bboxes[0,i,4]):.2f}",(cx,cy), color='r')
+            w = bboxes[0, i, 2] * 320
+            h = bboxes[0, i, 3] * 320
 
-    if target:
-        for i in range(target.shape[1]):
-            if target[0,i,-1] > 0:
-                cx = int(target[0,i,0]*img_shape - target[0,i,2]*img_shape/2) + pad
-                cy = int(target[0,i,1]*img_shape - target[0,i,3]*img_shape/2) + pad
+            rect = patches.Rectangle((cx, cy),
+                                     w, h, linewidth=2, facecolor='none', edgecolor='r')
+            ax.add_patch(rect)
+            ax.annotate(num_to_class(int(bboxes[0, i, 5])) + " " + f"{float(bboxes[0, i, 4]):.2f}", (cx, cy), color='r')
 
-                w = target[0,i,2]*img_shape
-                h = target[0,i,3]*img_shape
-
-                rect = patches.Rectangle((cx,cy),
-                                        w, h, linewidth=2, facecolor='none', edgecolor='g')
-                ax.add_patch(rect)
-                ax.annotate(num_to_class(int(target[0,i,5])),(cx,cy), color='g')
     plt.axis('off')
     if file_path is not None:
         plt.savefig(file_path, bbox_inches='tight')
@@ -85,14 +76,54 @@ def display_result(image: torch.Tensor, output: List[torch.tensor], target=None,
     plt.close()
 
 
-def plot_predictions(predictions, image, box_threshold=0.1, nms_threshold=0.25):
+def get_result(image: np.ndarray, output: List[np.ndarray], overwrite_class_idx=-1):
+    bboxes = np.stack(output, axis=0)
+    image = to_uint8_img(image)
+    image = image.copy()
+    img_size_y = image.shape[0]
+    img_size_x = image.shape[1]
+
+    for i in range(bboxes.shape[1]):
+        if bboxes[0, i, -1] > 0:
+            cx = math.floor(bboxes[0, i, 0] * img_size_x - bboxes[0, i, 2] * img_size_x // 2)
+            cy = math.floor(bboxes[0, i, 1] * img_size_y - bboxes[0, i, 3] * img_size_y // 2)
+            w = math.floor(bboxes[0, i, 2] * img_size_x)
+            h = math.floor(bboxes[0, i, 3] * img_size_y)
+            print(cx, cy, w, h)
+            cv2.rectangle(image, (cx, cy), (cx + w, cy + h), (255, 0, 0), 2)
+            if overwrite_class_idx == -1:
+                cv2.putText(image, num_to_class(int(bboxes[0, i, 5])) + " " + f"{float(bboxes[0, i, 4]):.2f}",
+                            (cx, cy - 6), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 0, 0), 1)
+            else:
+                cv2.putText(image, num_to_class(overwrite_class_idx) + " " + f"{float(bboxes[0, i, 4]):.2f}",
+                            (cx, cy - 6), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 0, 0), 1)
+
+    return image
+
+
+def plot_predictions(predictions, image, box_threshold=0.1, nms_threshold=0.25, return_array=False):
+    if isinstance(predictions, torch.Tensor):
+        predictions = predictions.numpy()
+    if isinstance(image, torch.Tensor):
+        image = image.numpy()
+        try:
+            image = np.squeeze(image, axis=0)
+        except:
+            pass
+        if image.shape[0] == 3:
+            image = image.transpose(1, 2, 0)
+
     # add batch dimensions
-    if len(predictions.size()) == 4:
-        predictions = torch.unsqueeze(predictions, 0)
-    if len(image.size()) == 3:
-        image = torch.unsqueeze(image, 0)
+    if len(predictions.shape) == 4:
+        predictions = np.expand_dims(predictions, axis=0)
 
     predictions = filter_boxes(predictions, box_threshold)
     # filter boxes based on overlap
     predictions = nms(predictions, nms_threshold)
-    display_result(image, predictions)
+    if return_array:
+        return get_result(image, predictions)
+    else:
+        display_result(image, predictions)
+
+
+
--- utils/yolo.py
+++ utils/yolo.py
@@ -1,54 +1,60 @@
-import torch
+import numpy as np
 from typing import List
+
 
 def iou(bboxes1, bboxes2):
     """ calculate iou between each bbox in `bboxes1` with each bbox in `bboxes2`"""
-    px, py, pw, ph = bboxes1[...,:4].reshape(-1, 4).split(1, -1)
-    lx, ly, lw, lh = bboxes2[...,:4].reshape(-1, 4).split(1, -1)
+    px, py, pw, ph = np.hsplit(bboxes1[...,:4].reshape(-1, 4), 4)
+    lx, ly, lw, lh = np.hsplit(bboxes2[...,:4].reshape(-1, 4), 4)
     px1, py1, px2, py2 = px - 0.5 * pw, py - 0.5 * ph, px + 0.5 * pw, py + 0.5 * ph
-    lx1, ly1, lx2, ly2 = lx - 0.5 * lw, ly - 0.5 * lh, lx + 0.5 * lw, ly + 0.5 * lh
-    zero = torch.tensor(0.0, dtype=px1.dtype, device=px1.device)
-    dx = torch.max(torch.min(px2, lx2.T) - torch.max(px1, lx1.T), zero)
-    dy = torch.max(torch.min(py2, ly2.T) - torch.max(py1, ly1.T), zero)
+    lx1, ly1, lx2, ly2 = lx - 0.5 * lw, ly - 0.5 * lh, lx + 0.5 * lw, ly + 0.5 * lh                
+    dx = np.maximum(np.minimum(px2, lx2.T) - np.maximum(px1, lx1.T), [0])
+    dy = np.maximum(np.minimum(py2, ly2.T) - np.maximum(py1, ly1.T), [0])
     intersections = dx * dy
     pa = (px2 - px1) * (py2 - py1) # area
     la = (lx2 - lx1) * (ly2 - ly1) # area
     unions = (pa + la.T) - intersections
     ious = (intersections/unions).reshape(*bboxes1.shape[:-1], *bboxes2.shape[:-1])
-    
+        
     return ious
 
-def nms(filtered_tensor: List[torch.Tensor], threshold: float) -> List[torch.Tensor]:
+
+def nms(filtered_array: List[np.ndarray], threshold: float) -> List[np.ndarray]:
     result = []
-    for x in filtered_tensor:
+    for x in filtered_array:
         # Sort coordinates by descending confidence
-        scores, order = x[:, 4].sort(0, descending=True)
+        scores = np.sort(x[:, 4])        
+        scores = scores[::-1]
+        order = np.argsort(x[:, 4])
+        order = order[::-1]        
         x = x[order]
         ious = iou(x,x) # get ious between each bbox in x
 
         # Filter based on iou
-        keep = (ious > threshold).long().triu(1).sum(0, keepdim=True).t().expand_as(x) == 0
+        keep = np.repeat(np.triu((ious > threshold).astype('int64'), 1).sum(0, keepdims=True).T, x.shape[1], axis=1) == 0
+        result.append(np.ascontiguousarray(x[keep].reshape(-1, 6)))
 
-        result.append(x[keep].view(-1, 6).contiguous())
     return result
 
 
-def filter_boxes(output_tensor: torch.Tensor, threshold) -> List[torch.Tensor]:
-    b, a, h, w, c = output_tensor.shape
-    x = output_tensor.contiguous().view(b, a * h * w, c)
+def filter_boxes(output_array: np.ndarray, threshold) -> List[np.ndarray]:
+    b, a, h, w, c = output_array.shape    
+    x = np.ascontiguousarray(output_array).reshape(b, a * h * w, c)
 
     boxes = x[:, :, 0:4]
-    confidence = x[:, :, 4]
-    scores, idx = torch.max(x[:, :, 5:], -1)
-    idx = idx.float()
+    confidence = x[:, :, 4]    
+    scores = np.max(x[:, :, 5:], -1)
+    idx = np.argmax(x[:, :, 5:], -1)
+    idx = idx.astype('float32')
     scores = scores * confidence
     mask = scores > threshold
 
     filtered = []
     for c, s, i, m in zip(boxes, scores, idx, mask):
         if m.any():
-            detected = torch.cat([c[m, :], s[m, None], i[m, None]], -1)
+            detected = np.concatenate([c[m, :], s[m, None], i[m, None]], -1)
         else:
-            detected = torch.zeros((0, 6), dtype=x.dtype, device=x.device)
+            detected = np.zeros((0, 6), dtype=x.dtype)
         filtered.append(detected)
+
     return filtered
